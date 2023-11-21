bafbb96bed660151a0adafb5c68401e684cbf4e6
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2023-11-21 19:43:21 +0200

    Keep Changes

--- src/sections/AuthenticationSection/AuthenticationSection.jsx
+++ src/sections/AuthenticationSection/AuthenticationSection.jsx
@@ -1,28 +1,45 @@
+"use client"
 import { PrimaryBox } from "@/MUIComponents/PrimaryBox";
 import { PrimaryContainer } from "@/MUIComponents/PrimaryContainer";
 import Form from "@/components/Form/Form";
 import Title from "@/components/Title/Title";
-import React from "react";
+import React, { useEffect } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import waitImg from "../../images/waiting.jpg"
+import { useParams, useRouter } from "next/navigation";
+import styles from "./AuthenticationSection.module.scss"
+import Cookies from "js-cookie";
 
 const AuthenticationSection = ({ type }) => {
+  const params = useParams()
+  const router = useRouter()
+  useEffect(() => {
+    try {
+      Cookies.set("AlCazar_token", params.token)
+      Cookies.set("AlCazar_userId", params.userId)
+      router.push("/")
+    } catch (err) {
+      console.log(err)
+    }
+  }, [params])
   return (
     <PrimaryBox component={"section"}>
       <PrimaryContainer className={`grid jcs aic g30`}>
-        <Title
+        {type !== "auth_provider" ? (<><Title
           title={`${type === "login"
-              ? "Login"
-              : type === "register"
-                ? "Register"
-                : type === "reset_password"
-                  ? "Reset Password"
-                  : type === "forgot_password" && "Forgot Password"
+            ? "Login"
+            : type === "register"
+              ? "Register"
+              : type === "reset_password"
+                ? "Reset Password"
+                : type === "forgot_password" && "Forgot Password"
             }`}
           line={true}
           h={"h3"}
           align={"center"}
           fw={600}
         />
-        <Form type={type} />
+          <Form type={type} /></>) : (<LazyLoadImage className={`center_rel_x ${styles.auth_image}`} src={waitImg.src} />)}
       </PrimaryContainer>
     </PrimaryBox>
   );
